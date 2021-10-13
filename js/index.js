17a4c843cc4f44f9f8cfae2410d5fff09440e645
Elrik Morvan <elrikmorvan@gmail.com>
2021-10-13 14:17:22 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const stars = () => {
+  const star = document.createElement("span");
+  document.body.appendChild(star);
+  star.classList.add("star");
+
+  const size = Math.random() * 2 + "px";
+
+  star.style.width = size;
+  star.style.height = size;
+
+  star.style.left = Math.random() * 100 + "%";
+  star.style.top = Math.random() * 100 + "%";
+
+  setTimeout(() => {
+    star.remove();
+  }, 5000);
+};
+
+setInterval(stars, 20);
+// stars();
